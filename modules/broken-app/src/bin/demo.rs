fdca7b5f821be343fb0820d4858cdaf1aeffe02c
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-03-08 12:55:31 +0400

    remove redundant operations from demo binary

--- modules/broken-app/src/bin/demo.rs
+++ modules/broken-app/src/bin/demo.rs
@@ -2,20 +2,23 @@ use broken_app::{algo, leak_buffer, normalize, sum_even, use_after_free};
 
 fn main() {
   let nums = [1, 2, 3, 4];
-  println!("sum_even: {}", sum_even(&nums));
+  // println!("sum_even: {}", sum_even(&nums));
+  sum_even(&nums);
 
   let data = [1_u8, 0, 2, 3];
-  println!("non-zero bytes: {}", leak_buffer(&data));
+  // println!("non-zero bytes: {}", leak_buffer(&data));
+  leak_buffer(&data);
 
   let text = " Hello World ";
-  println!("normalize: {}", normalize(text));
+  // println!("normalize: {}", normalize(text));
+  normalize(text);
 
   let fib = algo::slow_fib(20);
-  println!("fib(20): {}", fib);
+  // println!("fib(20): {}", fib);
 
   let uniq = algo::slow_dedup(&[1, 2, 2, 3, 1, 4, 4]);
-  println!("dedup: {:?}", uniq);
+  // println!("dedup: {:?}", uniq);
 
   let answer = unsafe { use_after_free() };
-  println!("use after free: {answer}");
+  // println!("use after free: {answer}");
 }
